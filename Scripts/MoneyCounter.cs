90dde7234ee7c45e4b57fd3f081ecccc9b82995a
SarahJoh <61331556+SarahJoh@users.noreply.github.com>
2020-08-01 09:24:28 +0200

    Add files via upload

---
+++ Scripts/MoneyCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyCounter : MonoBehaviour
+{
+    public static MoneyCounter Instance { set; get; }
+
+    Text text;
+    public int amount = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        text.text = amount.ToString();
+    }
+
+    // Reduces the players balance. 
+    public void ReduceAmount(int price)
+    {
+        amount -= price;
+    }
+}
